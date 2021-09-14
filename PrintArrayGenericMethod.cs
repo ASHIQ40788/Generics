ae251cc454deb20ddf623620bde04c30144e47e8
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-14 13:25:07 +0530

    [ASHIQ] added : Print_Array_Generic_Method File

---
+++ PrintArrayGenericMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class PrintArrayGenericMethod<T>
+    {
+        private T[] inputArray;
+        public PrintArrayGenericMethod(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+        public void toPrint()
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        
+       
+    }
+}
