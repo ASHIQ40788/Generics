c7fbd3c51ef11e05f6e34e7a87ce4b8771637398
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-14 13:22:57 +0530

    [ASHIQ] added : Print_Array_generic File

---
+++ PrintArrayGeneric.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class PrintArrayGeneric
+    {
+        public static void ToPrint<T>(T[] array)
+        {
+            Console.WriteLine("Displaying integer array elements");
+            foreach (var element in array)
+            {
+                Console.Write(element + "\t");
+            }
+        }
+    }
+}
