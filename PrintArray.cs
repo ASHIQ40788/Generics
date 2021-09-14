ab03d53b0ae43d53c9f421942d815ff6e962f45d
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-14 13:21:23 +0530

    [ASHIQ] added : Print_Array File

---
+++ PrintArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class PrintArray
+    {
+        public static void ToPrint(int[] intArray)
+        {
+            Console.WriteLine("Displaying integer array elements");
+            foreach (int element in intArray)
+            {
+                Console.Write(element +"\t");
+            }
+        }
+        public static void ToPrint(double[] intArray)
+        {
+            Console.WriteLine("Displaying double array elements");
+            foreach (double element in intArray)
+            {
+                Console.Write(element +"\t");
+            }
+
+
+        }
+        public static void ToPrint(char[] intArray)
+        {
+            Console.WriteLine("Displaying char array elements");
+            foreach (char element in intArray)
+            {
+                Console.Write(element + "\t");
+            }
+
+
+        }
+
+
+    }
+}
+
