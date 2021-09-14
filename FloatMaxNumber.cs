ffab74494b09aa7d8f4120269cbd75f861988674
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-14 12:27:16 +0530

    [ASHIQ] added : FLOAT_MAX_NUMBER

---
+++ FloatMaxNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class FloatMaxNumber
+    {
+        public static float FindMax(float first, float second, float third)
+        {
+         
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("2 or more nubers are equal");
+                return 0;
+            }
+
+        }
+    }
+}
